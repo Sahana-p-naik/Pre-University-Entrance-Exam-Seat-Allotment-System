7ea2c7528e8f53e50f663fa67fd719ccfce61a14
Sahana-p-naik <121450654+Sahana-p-naik@users.noreply.github.com>
2024-09-23 19:34:04 +0530

    connection.php

---
+++ connection.php
@@ -0,0 +1,18 @@
+<?php
+
+
+// Database connection parameters
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '********';
+$DATABASE_NAME = 'dbms_project';
+
+// Attempt to establish a connection to the database
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+
+// Check connection status
+if (mysqli_connect_errno()) {
+    // If connection failed, display an error message
+    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+?>
